ee213378e9aad4ee28296d7c61585b15f519c1ae
Gamzat <gamzik44@gmail.com>
2023-04-28 19:36:49 +0300

    Not adaptive

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private var LightIsOn: CGFloat = 1
+    private var lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
     }
-
-
+    
+    @IBAction func StartButtonTapped() {
+            startButton.setTitle("Next", for: .normal)
+        
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = LightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = LightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = LightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
 }
 
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
